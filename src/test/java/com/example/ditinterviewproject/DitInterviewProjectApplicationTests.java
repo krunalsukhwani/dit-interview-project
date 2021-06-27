9fe731895475b3b6781d733c08f94799dea8d611
Kunal <krunal.sukhwani@gmail.com>
2021-06-27 18:02:10 +0530

    Dit Interview Project fourth phase committed

--- src/test/java/com/example/ditinterviewproject/DitInterviewProjectApplicationTests.java
+++ src/test/java/com/example/ditinterviewproject/DitInterviewProjectApplicationTests.java
@@ -2,12 +2,25 @@ package com.example.ditinterviewproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-@SpringBootTest
+@SpringBootTest(classes = DitInterviewProjectApplicationTests.class)
 class DitInterviewProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	RequestBuilder validateAccessOfUserBasedOnToken()
+	{
+		return MockMvcRequestBuilders
+				.get("/hello")
+				.accept(MediaType.APPLICATION_JSON)
+				.header(HttpHeaders.AUTHORIZATION,"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2aXNocHRsIiwiZXhwIjoxNjI0ODE0OTE4LCJpYXQiOjE2MjQ3OTY5MTh9.6s1xkNFzQ0xUyhYv0NTmQUg_xQk2B3bhgNZC9ODiamqVQfWm0Zgzjo-jZM7cxPpNfhPtRXqck5ixBjT3iytT-A")
+				.contentType(MediaType.APPLICATION_JSON);
+	}
 }
