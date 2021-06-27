3c817747b2fafcd3df5b31b64af04692031e6f98
Kunal <krunal.sukhwani@gmail.com>
2021-06-27 18:47:14 +0530

    Dit Interview Project fourth phase committed

--- src/test/java/com/example/ditinterviewproject/DitInterviewProjectApplicationTests.java
+++ src/test/java/com/example/ditinterviewproject/DitInterviewProjectApplicationTests.java
@@ -1,26 +1,38 @@
 package com.example.ditinterviewproject;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest(classes = DitInterviewProjectApplicationTests.class)
+@AutoConfigureMockMvc
 class DitInterviewProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mvc;
 
-	@Test
-	RequestBuilder validateAccessOfUserBasedOnToken()
-	{
-		return MockMvcRequestBuilders
-				.get("/hello")
-				.accept(MediaType.APPLICATION_JSON)
-				.header(HttpHeaders.AUTHORIZATION,"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2aXNocHRsIiwiZXhwIjoxNjI0ODE0OTE4LCJpYXQiOjE2MjQ3OTY5MTh9.6s1xkNFzQ0xUyhYv0NTmQUg_xQk2B3bhgNZC9ODiamqVQfWm0Zgzjo-jZM7cxPpNfhPtRXqck5ixBjT3iytT-A")
-				.contentType(MediaType.APPLICATION_JSON);
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void shouldNotAllowAccessToUnauthenticatedUsers() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/hello")).andExpect(status().isOk());
+    }
+    @Test
+    public void shouldAllowAccessToAuthenticatedUsers() throws Exception {
+        mvc.perform(MockMvcRequestBuilders
+        .get("/hello")
+        .accept(MediaType.APPLICATION_JSON)
+        .header(HttpHeaders.AUTHORIZATION,"eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ2aXNocHRsIiwiZXhwIjoxNjI0ODE3NjM3LCJpYXQiOjE2MjQ3OTk2Mzd9.ykZr2zvmO5ZmOE2VoaR_MKW1tsTiSzl5f8hj8-z8LZftvlto7PZQcN_CKMlalCj8rGO6oCbnFaXabS546Arzzg")
+        .contentType(MediaType.APPLICATION_JSON));
+    }
 }
